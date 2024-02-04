a676fa68a5e0ddc6c70b73c2fac53597bc404ffe
mmuchsin <kangucin21@gmail.com>
2024-02-04 08:21:29 +0700

    feat: :sparkles: Add dynamic highlighting for active player

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,23 +1,40 @@
+import { useState } from "react";
+
 const initialBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+  const [gameBoard, setGameBoard] = useState(initialBoard);
+
+  function handleGameBoardClik(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      // deepcopy agar ketika newGameBoard diubah prevGameBoard tidak ikut berubah
+      const newGameBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      newGameBoard[rowIndex][colIndex] =  activePlayerSymbol;
+      return newGameBoard;
+    });
+    onSelectSquare();
+  }
   return (
     <ol id="game-board">
-      {initialBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((col, colIndex) => (
               <li key={colIndex}>
-                <button>{col}</button>
+                <button onClick={() => handleGameBoardClik(rowIndex, colIndex)}>
+                  {col}
+                </button>
               </li>
-      ))}
+            ))}
           </ol>
         </li>
-     ))}
+      ))}
     </ol>
   );
 }
