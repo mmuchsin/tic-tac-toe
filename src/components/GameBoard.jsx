cc42224187209858b0f256bf362b50e959bc124f
mmuchsin <kangucin21@gmail.com>
2024-02-04 09:59:17 +0700

    docs: :memo: add GameBoard docs with cody

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
 
+/**
+ * Defines the initial state of the game board,
+ * with all squares set to null.
+ */
 const initialBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
+/**
+ * Renders a tic-tac-toe game board with clickable squares to place moves.
+ *
+ * @param {Function} onSelectSquare - Callback when a square is clicked
+ * @param {string} activePlayerSymbol - 'X' or 'O' for the current player
+ */
 export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initialBoard);
 
@@ -15,7 +25,7 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
       const newGameBoard = [
         ...prevGameBoard.map((innerArray) => [...innerArray]),
       ];
-      newGameBoard[rowIndex][colIndex] =  activePlayerSymbol;
+      newGameBoard[rowIndex][colIndex] = activePlayerSymbol;
       return newGameBoard;
     });
     onSelectSquare();
